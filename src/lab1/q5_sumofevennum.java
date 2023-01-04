1d0f0d629885dd7a13d8e6c687bb79b043db5d47
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-04 21:47:51 +0530

    commit for lab1 without practice questions

---
+++ src/lab1/q5_sumofevennum.java
@@ -0,0 +1,33 @@
+package lab1;
+import java.util.Scanner;
+import java.lang.*;
+public class q5_sumofevennum {
+    public static int calculateSumOfEvenNumbers(int[] arro){
+        int total=0;
+
+        for(int a:arro){
+            if(a%2 == 0){
+                total+=a;
+            }
+        }
+        return total;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers(in one line):");
+        String str="";
+        while (true) {
+            try {
+                str = scanner.nextLine();
+            } finally {
+                break;
+            }
+        }
+        String[] string = str.split(" ");
+        int[] arr = new int[string.length];
+        for (int i = 0; i < string.length; i++) {
+            arr[i] = Integer.valueOf(string[i]);
+        }
+        System.out.println("Sum of Even Numbers:" + calculateSumOfEvenNumbers(arr));
+    }
+}
