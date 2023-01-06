b53573f64a08a79e8b8ad229509f4dc791637f6e
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-06 20:55:27 +0530

    lab2 started

---
+++ src/lab2/q1_SubstringCounter.java
@@ -0,0 +1,31 @@
+package lab2;
+
+import java.util.Scanner;
+public class q1_SubstringCounter {
+    public static Scanner sc;
+    public static void main(String[] args) {
+        System.out.println("Enter Username:");
+        sc = new Scanner(System.in);
+        String inputString= sc.nextLine();
+        System.out.println(inputString.length());
+        int rn = 0,substrings=0;
+        for(int i=0;i<inputString.length();i++){
+            if(rn==0){
+                if('h'==inputString.charAt(i)){
+                    rn=1;
+                }
+            }
+            if(rn==1){
+                if('i'==inputString.charAt(i)){
+                    rn=0;
+                    substrings++;
+                }
+                else if ('h' !=inputString.charAt(i)){
+                    rn=0;
+                }
+            }
+        }
+        System.out.println(substrings);
+    }
+}
+
