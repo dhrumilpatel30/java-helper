f1e41f570d0df85d166b67f596ea2bd0d4f4e5b6
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-06 21:06:33 +0530

    lab2 add more

---
+++ src/lab2/q2_pallidrome_checker.java
@@ -0,0 +1,26 @@
+package lab2;
+
+import java.util.Scanner;
+public class q2_pallidrome_checker {
+    public static Scanner sc;
+    public static void main(String[] args) {
+        System.out.println("Enter String:");
+        sc = new Scanner(System.in);
+        String inputString= sc.nextLine();
+        String status="is pallidrome";
+        int half_length;
+        if(inputString.length()%2 == 0){
+            half_length=inputString.length()/2;
+        }
+        else{
+            half_length=(inputString.length()+1)/2;
+        }
+        for(int i=0;i<half_length;i++){
+            if(inputString.charAt(i)!=inputString.charAt(inputString.length()-i -1)){
+                status="not a pallidrome";
+            }
+
+        }
+        System.out.println("The Strings is "+status);
+    }
+}
