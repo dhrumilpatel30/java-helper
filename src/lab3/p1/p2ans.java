7b34c71110a55dee3ee93ffe6eb18557324cc874
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-08 22:11:50 +0530

    Employee class solution added

--- src/lab3/p1/p2ans.java
+++ src/lab3/p1/p2ans.java
@@ -2,35 +2,34 @@ package lab3.p1;
 import java.util.*;
 public class p2ans {
     public static void main(String[] args) {
-        Employee[] st=new Employee[3];
-        double avg,maxavg=0;
+        Employee[] em=new Employee[10];
+        double max_salary=0;
         int idex=0;
-        for(int i=0;i<3;i++){
+        for(int i=0;i<10;i++){
             Scanner sc =new Scanner(System.in);
-            System.out.print("Enter roll no of Student No."+(i+1)+": ");
+            System.out.print("Enter id No."+(i+1)+": ");
             int rol= sc.nextInt();
-            sc.nextLine();
-            System.out.print("Enter name of Student No."+(i+1)+": ");
+            System.out.print("Enter name No."+(i+1)+": ");
             String name= sc.nextLine();
-            System.out.print("Enter age of Student No."+(i+1)+": ");
+            System.out.print("Enter age No."+(i+1)+": ");
             int age=sc.nextInt();
-            System.out.print("Enter no of subjects of Student No."+(i+1)+": ");
-            int sub=sc.nextInt();
-            double[] marks= new double[sub];
-            avg=0;
-            System.out.print("Enter marks of Student No."+(i+1)+": ");
-            for(int j=0;j<sub;j++){
-                marks[j]=sc.nextInt();
-                avg=avg+marks[j];
-            }
-            avg/=sub;
-            if(avg>maxavg){
-                maxavg=avg;
+            System.out.print("Enter Department No."+(i+1)+": ");
+            String dept=sc.nextLine();
+            System.out.print("Enter Gross salary No."+(i+1)+": ");
+            double gs=sc.nextDouble();
+            System.out.print("Enter Provident Fund No."+(i+1)+": ");
+            double pf=sc.nextDouble();
+            System.out.print("Enter Income Tax No."+(i+1)+": ");
+            double it=sc.nextDouble();
+            System.out.print("Enter Professional tax No."+(i+1)+": ");
+            double pt=sc.nextDouble();
+            em[i]=new Employee(rol,name,age,gs,pf,it,pt,dept);
+            if(em[i].calculateSalary()>max_salary){
+                max_salary=em[i].calculateSalary();
                 idex=i;
             }
-            st[i]=new Employee(rol,marks,age,name);
         }
-        st[idex].displayDetails();
+        em[idex].displayDetails();
         Employee st1=new Employee();
         System.out.println("The Number of Students Created="+Employee.getCount());
     }
