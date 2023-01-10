19eb237c3fa5d63176e46aa127ad6384d8e07538
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-10 21:35:20 +0530

    lab4 more ques

--- src/lab4/q2_multiplecatch.java
+++ src/lab4/q2_multiplecatch.java
@@ -5,14 +5,22 @@ public class q2_multiplecatch {
        int[] a = new int[5];
        try {
            a[5]=30/5;
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
+       } catch (ArrayIndexOutOfBoundsException e) {
+           System.out.println(e);
        }
        a[0] = 0;
        try {
            a[4]=a[3]/a[0];
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
+       } catch (ArithmeticException e) {
+           System.out.println(e);
        }
+       //both together
+        try {
+            a[5]=a[3]/a[0];
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
     }
 }
