8447b0dd174f33dfa374086646e844597d0d2b10
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-11 20:41:18 +0530

    q4 added in lab4

---
+++ src/lab4/q4/Account.java
@@ -0,0 +1,6 @@
+package lab4.q4;
+
+public interface Account {
+    public void deposite(double amount);
+    public void withdraw(double amount);
+}
---
+++ src/lab4/q4/SavingsAccount.java
@@ -0,0 +1,13 @@
+package lab4.q4;
+
+public class SavingsAccount implements Account{
+    @Override
+    public void deposite(double amount) {
+
+    }
+
+    @Override
+    public void withdraw(double amount) {
+
+    }
+}
