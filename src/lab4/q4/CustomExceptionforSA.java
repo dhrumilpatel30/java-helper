b75b69aca132172e4fe570a50a1c7fa0c3ba5926
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-11 20:54:30 +0530

    q4 add in lab4

---
+++ src/lab4/q4/CustomExceptionforSA.java
@@ -0,0 +1,7 @@
+package lab4.q4;
+
+public class CustomExceptionforSA extends Exception{
+    CustomExceptionforSA(String message){
+        super(message);
+    }
+}
--- src/lab4/q4/SavingsAccount.java
+++ src/lab4/q4/SavingsAccount.java
@@ -1,13 +1,29 @@
 package lab4.q4;
 
 public class SavingsAccount implements Account{
+    double available_balance=0;
     @Override
     public void deposite(double amount) {
-
+        try{
+            if(amount < 0){
+                throw new CustomExceptionforSA("The deposite ammount should be positive");
+            }
+            available_balance+=amount;
+        }catch (CustomExceptionforSA ce){
+            ce.printStackTrace();
+        }
     }
 
     @Override
     public void withdraw(double amount) {
 
+        try{
+            if(amount < 0){
+                throw new CustomExceptionforSA("The deposite ammount should be positive");
+            }
+            available_balance+=amount;
+        }catch (CustomExceptionforSA ce){
+            ce.printStackTrace();
+        }
     }
 }
