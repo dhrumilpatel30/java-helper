9c8f0846b60cbbe135691d63237588e65ef68303
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-18 20:56:21 +0530

    lab5 q1 end

---
+++ src/lab5/UndergraduateStudent.java
@@ -0,0 +1,18 @@
+package lab5;
+
+public class UndergraduateStudent extends Student{
+    public UndergraduateStudent(String name) {
+		super(name);
+	}
+
+	public void generateResult() {
+		double avg=calculateavg();
+		if(avg<60) {
+			setTestResult("Fail");
+		}
+		else {
+			setTestResult("Pass");
+		}
+	}
+
+}
