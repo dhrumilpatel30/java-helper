5f08adbd586a91307f0fde1165ced8cc7fd5a582
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-18 20:55:19 +0530

    lab5 q1 start

---
+++ src/lab5/Student.java
@@ -0,0 +1,51 @@
+package lab5;
+
+import java.util.Arrays;
+
+public abstract class Student {
+    private String studentName;
+	private int[] testScores = new int[10];
+	private String testResult;
+
+	public Student(String name) {
+		this.studentName = name;
+		Arrays.fill(testScores, -1);
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int[] getTestScores() {
+		return testScores;
+	}
+
+	public void setTestScore(int testNumber, int testScore) {
+		this.testScores[testNumber - 1] = testScore;
+	}
+
+	public String getTestResult() {
+		return testResult;
+	}
+
+	public void setTestResult(String testResult) {
+		this.testResult = testResult;
+	}
+
+	public double calculateavg() {
+		double avg = 0, i1 = 0;
+		for (int i : testScores) {
+			if (-1 != i) {
+				avg += i;
+				i1 += 1;
+			}
+		}
+		return avg / i1;
+	}
+
+	abstract public void generateResult();
+}
--- src/lab5/q1_ans.java
+++ src/lab5/q1_ans.java
@@ -1,4 +1,13 @@
 package lab5;
 
 public class q1_ans {
+    public static void main(String[] args) {
+		UndergraduateStudent ugst1 = new UndergraduateStudent("Jerry");
+		ugst1.setTestScore(1,70);
+		ugst1.setTestScore(2,69);
+		ugst1.setTestScore(3,71);
+		ugst1.setTestScore(4,55);
+		ugst1.generateResult();
+		System.out.println(ugst1.getTestResult());
+	}
 }
