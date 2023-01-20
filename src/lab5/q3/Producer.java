44807e59583ae42ed57db61bb007b676cc4274b0
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-20 21:28:20 +0530

    q3 modify

---
+++ src/lab5/q3/Producer.java
@@ -0,0 +1,18 @@
+package lab5.q3;
+
+public class Producer implements Runnable{
+    	Queue1 q1;
+
+	public Producer(Queue1 rough) {
+		this.q1=rough;
+		new Thread(this, "Producer").start();
+	}
+	@Override
+	public void run() {
+		int i = 0;
+        while (true) {
+            q1.enqueue(i++);
+            System.out.println("Pushed "+i);
+        }
+	}
+}
