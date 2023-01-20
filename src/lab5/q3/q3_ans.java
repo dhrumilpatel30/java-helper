40f88ba666485dd64a8a5d8643daf8f80e8715a9
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-20 21:31:39 +0530

    q3 modify

---
+++ src/lab5/q3/q3_ans.java
@@ -0,0 +1,12 @@
+package lab5.q3;
+
+public class q3_ans {
+    public static void main(String[] args) {
+		Queue1 q1 =  new Queue1(10);
+
+		new Producer(q1);
+		new Consumer(q1);
+		System.out.println("Queue with overflow and underflow protection...");
+
+	}
+}
