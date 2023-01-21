0072c821aab4b7ebd962de56f3df73c7bec9e3ac
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-21 20:57:20 +0530

    action of deadlock completed

--- src/lab5/q4/q4_ans.java
+++ src/lab5/q4/q4_ans.java
@@ -2,6 +2,7 @@ package lab5.q4;
 
 public class q4_ans {
     public static void main(String[] args) {
+
         Deadlock d1=new Deadlock();
 
         System.out.println("Deadlock never ends...");
