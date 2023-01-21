1c60b1c1f42f3e100f9da25cee36d4e0b93eb03b
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-21 20:35:27 +0530

    deadlock in action

--- src/lab5/q4/Deadlock.java
+++ src/lab5/q4/Deadlock.java
@@ -1,5 +1,29 @@
 package lab5.q4;
 
-public class Deadlock {
-    
+
+class A{
+
+    synchronized void methodofA(B b){
+
+    }
+    synchronized void isAacquired(){
+        System.out.println("Lock for A is acquired...");
+    }
+}
+class B{
+    synchronized void methodofB(A a){
+
+    }
+    synchronized void isBacquired(){
+        System.out.println("Lock for B is acquired...");
+    }
+}
+public class Deadlock implements Runnable{
+    public A a;
+    public B b;
+
+    @Override
+    public void run() {
+
+    }
 }
---
+++ src/lab5/q4/q4_ans.java
@@ -0,0 +1,7 @@
+package lab5.q4;
+
+public class q4_ans {
+    public static void main(String[] args) {
+
+    }
+}
