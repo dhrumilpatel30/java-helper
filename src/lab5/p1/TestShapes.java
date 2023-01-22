88e651b02beb5900d3967b820c2a6d64a694c4ff
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-22 20:28:56 +0530

    lab5/p1 modify

--- src/lab5/p1/TestShapes.java
+++ src/lab5/p1/TestShapes.java
@@ -2,6 +2,24 @@ package lab5.p1;
 
 public class TestShapes {
 
+    static void shapeInfo(Shape obj){
+        if(obj instanceof Circle){
+            System.out.println(obj.toString());
+            System.out.println("Radius="+ ((Circle) obj).getRadius());
+        }
+        else if(obj instanceof Square){
+            System.out.println(obj.toString());
+            System.out.println("Length is =" + ((Square) obj).getLength());
+        }
+        else if(obj instanceof Rectangle){
+            System.out.println(obj.toString());
+            System.out.println("The length and breath are"+((Rectangle) obj).getLength()+" "+ ((Rectangle) obj).getBreadth());
+        }
+        else{
+            System.out.println(obj.toString());
+            System.out.println("Shape is undefined");
+        }
+    }
     public static void main(String[] args) {
         Circle c1 = new Circle();
         Rectangle r1 = new Rectangle();
