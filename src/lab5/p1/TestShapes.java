fb13ea9fdfde594493394ed6bfcfd8689150c847
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-22 20:24:09 +0530

    lab5/p1 modify

--- src/lab5/p1/TestShapes.java
+++ src/lab5/p1/TestShapes.java
@@ -1,5 +1,26 @@
 package lab5.p1;
 
 public class TestShapes {
-    Circle c1 = new Circle();
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Rectangle r1 = new Rectangle();
+        Square s1 = new Square(2.2);
+
+        c1.setRadius(2.2);
+        c1.findArea();
+        c1.showArea();
+
+        System.out.println(s1.getLength());
+        s1.findArea();
+        s1.showArea();
+
+        r1.setLength(1.2);
+        r1.setBreadth(2.1);
+        r1.findArea();
+        r1.showArea();
+
+        Rectangle r2 = s1;
+        System.out.println(r2.toString());
+    }
 }
