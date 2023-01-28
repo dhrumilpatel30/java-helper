0061ca098c6903a0f6a92b49f22c6577e98aa94a
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-28 18:41:14 +0530

    jdbc student registration demo init commit

--- src/lab6/q1/q1_ans.java
+++ src/lab6/q1/q1_ans.java
@@ -1,14 +1,13 @@
 package lab6.q1;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.text.*;
 import java.util.Scanner;
 
 public class q1_ans {//Student Registration form
     private static Scanner sc = new Scanner(System.in);
-
-    public static void print_from_database(Statement s) throws SQLException {
+    public static void print_from_database(Connection con) throws SQLException {
+        Statement s = con.createStatement();
         String selectQuery = "select * from `student`";
         ResultSet rs;
         rs = s.executeQuery(selectQuery);
@@ -21,6 +20,7 @@ public class q1_ans {//Student Registration form
             System.out.println("Branch: " + rs.getString(3));
             System.out.println("Username: " + rs.getString(4));
             System.out.println("Password: " + rs.getString(5));
+            System.out.println("........................");
         }
     }
 
@@ -64,41 +64,77 @@ public class q1_ans {//Student Registration form
     }
 
     public static void Update_in_database(Connection con) throws SQLException{
-
+        System.out.print("Update of row started...\nEnter the id for update: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        Statement s = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = s.executeQuery("select * from student where id="+id);
+        System.out.println("Enter new Details..");
+        rs.next();
+        System.out.print("Enter the First Name: ");
+        String fname = sc.nextLine();
+        System.out.print("Enter the Last Name: ");
+        String lname = sc.nextLine();
+        System.out.print("Enter the Branch: ");
+        String branch = sc.nextLine();
+        System.out.print("Enter the Username: ");
+        String username = sc.nextLine();
+        System.out.print("Enter the password: ");
+        String password = sc.nextLine();
+        rs.updateString(1,fname);
+        rs.updateString(2,lname);
+        rs.updateString(3,branch);
+        rs.updateString(4,username);
+        rs.updateString(5,password);
+        rs.updateRow();
+        System.out.println("Row successfully updated..");
     }
 
     public static void Delete_from_database(Connection con) throws SQLException{
-
+        System.out.print("Delete of row started...\nEnter the id for Delete");
+        int id = sc.nextInt();
+        sc.nextLine();
+        Statement s = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = s.executeQuery("select * from student where id="+id);
+        rs.absolute(id);
+        rs.deleteRow();
+        System.out.println("Row successfully deleted");
     }
     public static void selector(Connection con) throws SQLException {
         //Giving User to choose the option for manipulation of data
             System.out.println("Give your key according to choice.");
+            System.out.println("View the data : 0");
             System.out.println("Insert a Row.: 1");
             System.out.println("Update a Row.: 2");
             System.out.println("Delete a Row.: 3");
-            System.out.println("Multiple Insertion.: 4");
+            System.out.print("Multiple Insertion.: 4\nkey:");
             int choice = sc.nextInt();
+            sc.nextLine();
+            if(0 == choice)print_from_database(con);
             if(1 == choice)Insert_in_database(con);
             if(2 == choice)Update_in_database(con);
             if(3 == choice)Delete_from_database(con);
-            if(4 == choice){
-                while(0 == choice--){
+            if(4 == choice) {
+                while (0 == choice--) {
                     Insert_in_database(con);
                 }
             }
-            System.out.print("Do you want to exit (y/n): ");
-            if("y" == sc.nextLine())selector(con);
     }
 
     public static void main(String args[]) {
         //setting up database
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/student-registration", "root", "")) {
-            Statement s = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
             //initially showing information already present in database
-            print_from_database(s);
+            print_from_database(con);
             //starting selections
             selector(con);
+            while(true){
+                System.out.print("Do you want to exit (y/n): ");
+                String s = sc.nextLine();
+                System.out.println(s);
+                if("y" == s)break;
+                selector(con);
+            }
         } catch (SQLException e) {
             System.out.println(e);
         }
